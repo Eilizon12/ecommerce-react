572399abcae0104911e7b680e3521d7495c1cfe6
Eilizon12 <Eilizon12agcaoili@gmail.com>
2023-11-25 19:36:01 +0800

    sds

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,4 +2,6 @@ const express = require("express");
 const { registerUser } = require("../controllers/userController");
 const router = express.Router();
 
-router.route("/register").post(registerUser)
+router.route("/register").post(registerUser);
+
+module.exports = router;
