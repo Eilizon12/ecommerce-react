a98f9a2c416c4d007ccf2cabb7f280cbbf990b58
Eilizon12 <Eilizon12agcaoili@gmail.com>
2023-11-25 22:29:46 +0800

    paano ba ayusin yung email send

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,24 +1,22 @@
 const nodeMailer = require("nodemailer");
 
 const sendEmail = async(options)=>{
-
         const transporter = nodeMailer.createTransport({
+            service: process.env.SMPT_SERVICE,
+            auth: {
+                user: process.env.SMPT_MAIL,
+                pass: process.env.SMPT_PASSWORD,
+            },
 
-            service:process.env.SMPT_SERVICE,
-            auth:{
-                user:process.env.SMPT_MAIL,
-                password:process.env.SMPT_PASSWORD,
-            }
-
-        })
+        });
 
         const mailOptions = {
-            from:process.env.SMPT_MAIL,
+            from: process.env.SMPT_MAIL,
             to:options.email,
             subject:options.subject,
-            text:options.message,
+            text: options.message,
         };
-await transporter.sendMail(mailOptions);
+        await transporter.sendMail(mailOptions);
 };
 
 module.exports = sendEmail;
