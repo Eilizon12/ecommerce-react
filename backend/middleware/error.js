64b484ce503580cace4be163fb157446ae5f0ca6
Eilizon12 <Eilizon12agcaoili@gmail.com>
2023-11-25 23:47:51 +0800

    SDS

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -11,6 +11,23 @@ module.exports = (err,req,res,next)=>{
         err = new ErrorHandler(message,400);
     }
 
+    //Mongoose Duplicate
+    if(err.code === 11000){
+        const message = `Duplicate ${Object.keys(err.keyValue)} Entered`
+        err = new ErrorHandler(message,400);
+    };
+
+    // Wrong JWT error
+    if(err.code === "JsonWebTokenError"){
+        const message = `Json Web Token is Invalid!, Try Again!.`
+        err = new ErrorHandler(message,400);
+    };
+    // JWT Expire
+    if(err.code === "JsonWebExpiredError"){
+        const message = `Json Web Token is EXPIRED!, Try Again!.`
+        err = new ErrorHandler(message,400);
+    };
+
     res.status(err.statusCode).json({
         success:false,
         error: err.message,
