950783bb252600a5a24510430b2c33b4f3ebb02a
Eilizon12 <Eilizon12agcaoili@gmail.com>
2023-11-26 14:20:37 +0800

    yoko na

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Headers from "./component/layout/Header.js";
+import {BrowserRouter as Router} from "react-router-dom"
+
 
 function App() {
   return (
-
+  <Router>
     <Headers />
+  </Router>
   );
+  
 }
 
 export default App;
