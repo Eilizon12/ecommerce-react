a800c5a0748b7bb7dd991cae5867ec233f39fe9f
Eilizon12 <Eilizon12agcaoili@gmail.com>
2023-11-26 12:17:44 +0800

    sdsd

---
+++ frontend/src/components/layout/Header.js
@@ -0,0 +1,11 @@
+// Header.js
+
+import React from 'react';
+import {ReactNavbar} from "overlay-navbar"
+
+const Header = () => {
+  // Your header component implementation
+  return  <ReactNavbar />; 
+};
+
+export { Header }; // Exporting the Header component
