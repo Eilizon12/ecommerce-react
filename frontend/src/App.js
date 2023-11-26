c86d95951ebd956401e7d5c18be39ce989eb85ee
Eilizon12 <Eilizon12agcaoili@gmail.com>
2023-11-26 21:18:29 +0800

    sdsd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,12 @@
-import React, { useEffect } from 'react';
-import Headers from './component/layout/Header/Header.js';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import WebFont from 'webfontloader';
-import Footer from './component/layout/Footer/Footer.js';
-import Home from './component/Home/Home.js';
+import './App.css';
+import Header from "./component/layout/Header.js";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
-  useEffect(() => {
-    WebFont.load({
-      google: {
-        families: ['Roboto', 'Droid Sans', 'Chilanka'],
-      },
-    });
-  }, []);
-
-  return (
-    <Router>
-      <Headers />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-      </Routes>
-      <Footer />
-    </Router>
-  );
+  return  (<Router>
+    <Header />
+  </Router>);
+  
 }
 
 export default App;
