dfc465f5676c2c0e78cd0a2869218ad929e9d825
Eilizon12 <Eilizon12agcaoili@gmail.com>
2023-11-26 16:57:32 +0800

    haysss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import Headers from "./component/layout/Header/Header.js";
-import { BrowserRouter as Router } from "react-router-dom";
-import WebFont from "webfontloader";
-import Footer from "./component/layout/Footer/Footer.js";
+import React, { useEffect } from 'react';
+import Headers from './component/layout/Header/Header.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import WebFont from 'webfontloader';
+import Footer from './component/layout/Footer/Footer.js';
+import Home from './component/Home/Home.js';
 
-class App extends Component {
-  componentDidMount() {
+function App() {
+  useEffect(() => {
     WebFont.load({
       google: {
-        families: ["Roboto", "Droid Sans", "Chilanka"]
-      }
+        families: ['Roboto', 'Droid Sans', 'Chilanka'],
+      },
     });
-  }
+  }, []);
 
-  render() {
-    return (
-      <Router>
-        <Headers />
-
-
-        <Footer />
-      </Router>
-    );
-  }
+  return (
+    <Router>
+      <Headers />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
--- frontend/src/component/layout/Footer/Footer.js
+++ frontend/src/component/layout/Footer/Footer.js
@@ -7,19 +7,19 @@ const Footer = () => {
   return (
 
     <footer id = "footer">
-        <div class="leftFooter">
+        <div className="leftFooter">
             <h4>Download Now</h4>
             <p>Download App for IOS and Android</p>
             <img src = {playStore} alt="playstore" />
             <img src = {appStore} alt="playstore" />
 
         </div>
-        <div class="midFooter">
+        <div className="midFooter">
             <h1>ECOMMERCE REACT</h1>
             <p>Quality Product</p>
             <p>Copy Rights 2001 &copy; Eilizon Agcaoili</p>
         </div>
-        <div class="rightFooter">
+        <div className="rightFooter">
             <h3>Follow Us</h3>
             <a href="http://instagram.com/zoneyli">Instagram</a>
 
